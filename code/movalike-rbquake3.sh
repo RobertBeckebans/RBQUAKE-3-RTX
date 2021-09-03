13a41b8faed929448743e508bd495db8d54ee3b0
Robert Beckebans <robert.beckebans@gmail.com>
2021-09-03 17:06:25 +0200

    Updated layout script

--- code/movalike-rbquake3.sh
+++ code/movalike-rbquake3.sh
@@ -9,12 +9,21 @@ mkdir -p libs
 mkdir -p engine
 mkdir -p engine/null
 mkdir -p games/q3a
+mkdir -p shared
 
 git mv jpeg-6 libs/jpeg
 #mv AL libs/openal
 
 #mv gameshared shared
 git mv botlib shared/botlib
+
+git mv game/bg_public.h shared/
+git mv cgame/cg_public.h shared/
+git mv game/g_public.h shared/
+git mv game/q_math.c shared/
+git mv game/q_shared.c shared/
+git mv game/q_shared.h shared/
+git mv game/surfaceflags.h shared/
 
 git mv qcommon engine/
 git mv client engine/
@@ -26,4 +35,4 @@ git mv server engine/
 
 git mv cgame games/q3a/
 git mv game games/q3a/
-git mv ui games/q3a/
+git mv ui games/q3a/q3_ui
